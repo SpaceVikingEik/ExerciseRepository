440308d9a8657a2f22af57130ff34c6f48e0693f
eikbo <eikb@itu.dk>
2022-09-06 17:07:50 +0200

    Created User Interface

--- ExerciseRepository/Program.cs
+++ ExerciseRepository/Program.cs
@@ -4,6 +4,18 @@ public class Program{
 // See https://aka.ms/new-console-template for more information
 
 private static void Main(string[] args){
+Console.WriteLine("Enter Year:");
+
+string yearString = Console.ReadLine();
+int year = int.Parse(yearString);
+
+Program HahaThisIsABadTemplate = new Program();
+
+if (HahaThisIsABadTemplate.IsLeapYear(year)){
+    Console.WriteLine("yaaaaaay :33333");
+} else {
+    Console.WriteLine("nayyyyyy 333333:");
+}
 
 }
 public string HelloWorld(){
